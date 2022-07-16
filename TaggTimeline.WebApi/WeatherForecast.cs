d11d40dfc61247e8cd0dabc011e2460833029ae3
Pseudooo <mitchell.wright877@gmail.com>
2022-07-16 21:13:50 +0100

    Moved forecast generation to model class

--- TaggTimeline.WebApi/WeatherForecast.cs
+++ TaggTimeline.WebApi/WeatherForecast.cs
@@ -9,4 +9,20 @@ public class WeatherForecast
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+
+    public static List<WeatherForecast> GenerateRandom()
+    {
+        var Summaries = new string[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+    }
 }
