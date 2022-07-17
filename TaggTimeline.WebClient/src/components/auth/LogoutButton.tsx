7a68f83ab7ec73cf352943198924b10565b6f02c
Dan Wells <dantheradarman@gmail.com>
2022-07-17 13:40:25 +0100

    Created logout button

---
+++ TaggTimeline.WebClient/src/components/auth/LogoutButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@mui/material";
+import { FunctionComponent } from "react";
+import { useAppSelector } from "../../store/helper";
+
+export interface LogoutButtonProps {
+  /* If the button should hide when the user is logged in */
+  autoHide?: boolean;
+}
+
+/**
+ * Creates a logout button that redirects to the logout page.
+ * @param props The props for this component
+ */
+export const LogoutButton: FunctionComponent<LogoutButtonProps> = ({
+  autoHide = true,
+}) => {
+  const loggedIn = useAppSelector((state) => state.auth.loggedIn);
+
+  if (!loggedIn && autoHide) {
+    return null;
+  }
+
+  return (
+    <Button href="/login" color="inherit">
+      Log Out
+    </Button>
+  );
+};
+
+export default LogoutButton;
