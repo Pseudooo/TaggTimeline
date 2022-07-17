59885df69861d90a13f1b13c4924766fd2bf41a4
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 21:41:30 +0100

    Added fluent validation pipeline

--- TaggTimeline.Service/ServiceCollectionExtensions.cs
+++ TaggTimeline.Service/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using TaggTimeline.Service.PipelineBehaviours;
 
 namespace TaggTimeline.Service;
 
@@ -9,6 +11,9 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddServiceDependencies(this IServiceCollection sc)
     {
         sc.AddMediatR(Assembly.GetExecutingAssembly());
+        sc.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        sc.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
         return sc;
     }
