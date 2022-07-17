fb6f6a5aa3f493ee7e34b50a7cc0e196a748d32e
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 12:25:36 +0100

    Modified to use mediatr from service layer

--- TaggTimeline.WebApi/Controllers/WeatherForecastController.cs
+++ TaggTimeline.WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using TaggTimeline.Service.Queries;
 
 namespace TaggTimeline.WebApi.Controllers;
 
@@ -6,27 +8,20 @@ namespace TaggTimeline.WebApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IMediator _mediatr;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediatr)
     {
         _logger = logger;
+        _mediatr = mediatr;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<IActionResult> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var query = new GetAllWeatherForecastsQuery();
+        var result = await _mediatr.Send(query);
+        return Ok(result);
     }
 }
