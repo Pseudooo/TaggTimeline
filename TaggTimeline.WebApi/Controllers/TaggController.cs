0776d4f9c6bd92b0f4d9aab5437b13495271ed08
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 15:22:32 +0100

    Mocked out init endpoints

---
+++ TaggTimeline.WebApi/Controllers/TaggController.cs
@@ -0,0 +1,41 @@
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaggTimeline.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TaggController : ControllerBase
+{
+
+    private readonly IMediator _mediator;
+    private readonly ILogger<TaggController> _logger;
+
+    public TaggController(IMediator mediator, 
+                          ILogger<TaggController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllTaggs()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchForTag()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateNewTag()
+    {
+        throw new NotImplementedException();
+    }
+
+}
+
