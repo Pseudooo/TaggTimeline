586f355ea76f9d4dda80b4facab03446abef463b
Dan Wells <dantheradarman@gmail.com>
2022-07-17 12:47:03 +0100

    Removed menu button

--- TaggTimeline.WebClient/src/layouts/MainLayout/index.tsx
+++ TaggTimeline.WebClient/src/layouts/MainLayout/index.tsx
@@ -1,5 +1,4 @@
-import { Menu } from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
 
@@ -12,16 +11,16 @@ export const MainLayout: FunctionComponent = () => {
     <Box>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton
+          {/* <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
             <Menu />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" color="inherit" component="div">
-            TaggTimeline 2
+            TaggTimeline
           </Typography>
         </Toolbar>
       </AppBar>
