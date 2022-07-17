cebc119e4836ddf84e2905a3048a23557b37ee38
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 14:49:54 +0100

    Added migration runner project

---
+++ TaggTimeline.MigrationRunner/Configuration/DatabaseConfiguration.cs
@@ -0,0 +1,10 @@
+
+namespace TaggTimeline.MigrationRunner.Configuration;
+
+public class DatabaseConfiguration
+{
+
+    public string ConnectionString { get; set; }
+    public bool ForceRebuild { get; set; }
+
+}
