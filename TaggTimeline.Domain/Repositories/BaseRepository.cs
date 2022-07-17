242b2c651354c18a23fd153d10856d6c8c6cf734
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 18:21:29 +0100

    Added new base repo methods

--- TaggTimeline.Domain/Repositories/BaseRepository.cs
+++ TaggTimeline.Domain/Repositories/BaseRepository.cs
@@ -1,4 +1,5 @@
 
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using TaggTimeline.Domain.Entities;
 using TaggTimeline.Domain.Interface;
@@ -19,12 +20,25 @@ public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : 
         return _context.Set<TEntity>().SingleOrDefaultAsync(x => x.Id == id);
     }
 
+    public Task<TEntity?> GetByIdWithNavigationProperties(Guid id, params Expression<Func<TEntity, object>>[] exprs)
+    {
+        var query = _context.Set<TEntity>().AsQueryable();
+        query = exprs.Aggregate(query, (current, includes) => current.Include(includes));
+
+        return query.SingleOrDefaultAsync(x => x.Id == id);
+    }
+
     public async Task<TEntity> AddItem(TEntity entity)
     {
         await _context.AddAsync(entity);
         await _context.SaveChangesAsync(CancellationToken.None);
 
         return entity;
+    }
+
+    public Task SaveChanges(CancellationToken tok)
+    {
+        return _context.SaveChangesAsync(tok);
     }
 
 }
