74276dfa2735e278c51a9fb73839b125c91f8d14
Dan Wells <dantheradarman@gmail.com>
2022-07-17 12:01:04 +0100

    Removed unused boilerplate

--- TaggTimeline.WebClient/src/App.tsx
+++ TaggTimeline.WebClient/src/App.tsx
@@ -1,24 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            TaggTimeline
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
