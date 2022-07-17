de846a1655c0df5d36b6ac86cf2e29ca6c4cb846
Pseudooo <mitchell.wright877@gmail.com>
2022-07-17 16:07:16 +0100

    Created GetTaggById handler stuff

---
+++ TaggTimeline.Service/Handlers/GetTaggByIdHandler.cs
@@ -0,0 +1,23 @@
+
+using MediatR;
+using TaggTimeline.Domain.Entities.Taggs;
+using TaggTimeline.Domain.Interface;
+using TaggTimeline.Service.Queries;
+
+namespace TaggTimeline.Service.Handlers;
+
+public class GetTaggByIdHandler : IRequestHandler<GetTaggByIdQuery, Tagg?>
+{
+
+    private readonly IBaseRepository<Tagg> _baseRepository;
+
+    public GetTaggByIdHandler(IBaseRepository<Tagg> baseRepository)
+    {
+        _baseRepository = baseRepository;
+    }
+    
+    public Task<Tagg?> Handle(GetTaggByIdQuery request, CancellationToken cancellationToken)
+    {
+        return _baseRepository.GetById(request.Id);
+    }
+}
