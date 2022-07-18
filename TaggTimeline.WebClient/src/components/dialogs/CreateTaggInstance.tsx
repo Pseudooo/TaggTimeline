413df8bc943fc56a24f4b8b2ab47651f5565018b
Dan Wells <dantheradarman@gmail.com>
2022-07-18 11:31:32 +0100

    Further polish to create tagg dialog

--- TaggTimeline.WebClient/src/components/dialogs/CreateTaggInstance.tsx
+++ TaggTimeline.WebClient/src/components/dialogs/CreateTaggInstance.tsx
@@ -1,4 +1,5 @@
-import { Dialog, DialogProps, DialogTitle } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { Dialog, DialogProps, DialogTitle, IconButton } from "@mui/material";
 import { FunctionComponent } from "react";
 import { CreateTaggInstanceForm } from "../forms/CreateTaggInstance";
 
@@ -6,11 +7,23 @@ import { CreateTaggInstanceForm } from "../forms/CreateTaggInstance";
  * A dialog popup for creating a new tag instance
  * @param props The props for this component
  */
-export const CreateTaggInstanceDialog: FunctionComponent<DialogProps> = (
-  props
-) => {
+export const CreateTaggInstanceDialog: FunctionComponent<DialogProps> = ({
+  onClose,
+  ...props
+}) => {
   return (
-    <Dialog maxWidth="sm" fullWidth {...props}>
+    <Dialog maxWidth="sm" fullWidth onClose={onClose} {...props}>
+      <IconButton
+        aria-label="close"
+        onClick={() => {
+          if (onClose) {
+            onClose({}, "backdropClick");
+          }
+        }}
+        sx={{ position: "absolute", right: 8, top: 8 }}
+      >
+        <Close />
+      </IconButton>
       <DialogTitle>Add a Tagg</DialogTitle>
       <CreateTaggInstanceForm />
     </Dialog>
--- TaggTimeline.WebClient/src/components/forms/CreateTaggInstance.tsx
+++ TaggTimeline.WebClient/src/components/forms/CreateTaggInstance.tsx
@@ -1,21 +1,78 @@
-import { Typography } from "@mui/material";
+import { Circle } from "@mui/icons-material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  FormGroup,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { FunctionComponent, useState } from "react";
-import { Tagg } from "../../api/generated";
+import { TaggPreviewModel } from "../../api/generated";
 import { SelectTaggForm } from "./SelectTagg";
+import { stringToColour } from "../../util";
+import DatePicker from "../io/DatePicker";
+import { LoadingButton } from "@mui/lab";
+
+export interface CreateTaggInstanceForm {
+  onCancel?: () => void;
+}
 
 /**
  * Form for creating a new Tagg instance
  */
-export const CreateTaggInstanceForm: FunctionComponent = () => {
-  const [tagg, setTagg] = useState<Tagg>();
+export const CreateTaggInstanceForm: FunctionComponent<
+  CreateTaggInstanceForm
+> = ({ onCancel }) => {
+  const [tagg, setTagg] = useState<TaggPreviewModel>();
+  const [loading, setLoading] = useState(false);
 
-  const selectTagg = (tagg: Tagg) => {
+  const selectTagg = (tagg: TaggPreviewModel) => {
     setTagg(tagg);
+  };
+
+  const handleDateChange = () => {
+    // Do nothing
+  };
+
+  const tryCreateTaggInstance = (tagg: TaggPreviewModel) => {
+    setLoading(true);
   };
 
   return !tagg ? (
     <SelectTaggForm onSelected={selectTagg} />
   ) : (
-    <Typography>The selected tag is {tagg.key}</Typography>
+    <Card>
+      <ListItem dense>
+        <ListItemIcon>
+          <Circle sx={{ color: stringToColour(tagg.id ?? "") }} />
+        </ListItemIcon>
+        <ListItemText primary={tagg.key} secondary={tagg.id} />
+      </ListItem>
+      <CardContent>
+        <FormGroup>
+          <DatePicker
+            label="Date"
+            value={new Date()}
+            onChange={handleDateChange}
+            disabled
+          />
+        </FormGroup>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => setTagg(undefined)}>Back</Button>
+        <LoadingButton
+          loading={loading}
+          onClick={() => tryCreateTaggInstance(tagg)}
+          variant="outlined"
+        >
+          Create
+        </LoadingButton>
+      </CardActions>
+    </Card>
   );
 };
+
+export default CreateTaggInstanceForm;
