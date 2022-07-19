798dc23131463d8892942a9fb537aec0d4bae365
Pseudooo <mitchell.wright877@gmail.com>
2022-07-19 20:51:45 +0100

    Modified repo mocking structure

--- TaggTimeline.Service.Test/Mocks/MockTaggRepository.cs
+++ TaggTimeline.Service.Test/Mocks/MockTaggRepository.cs
@@ -1,7 +1,5 @@
 
-using System.Linq.Expressions;
 using Moq;
-using TaggTimeline.Domain.Entities.Taggs;
 using TaggTimeline.Domain.Interface;
 
 namespace TaggTimeline.Service.Test.Mocks;
@@ -9,49 +7,13 @@ namespace TaggTimeline.Service.Test.Mocks;
 public static class MockTaggRepository
 {
 
-    public static List<Tagg> InitialTaggs { get; private set; } = new List<Tagg>()
+    public static Mock<ITaggRepository> GetTaggRepository()
     {
-        new Tagg()
-            {
-                Id = Guid.NewGuid(),
-                Key = "TEST TAGG 1",
-                CreatedDate = DateTime.Now,
-                ModifiedDate = null,
-                DeletedDate = null,
-                Instances = Enumerable.Empty<Instance>()
-            },
-        new Tagg()
-            {
-                Id = Guid.NewGuid(),
-                Key = "TEST TAGG 2",
-                CreatedDate = DateTime.Now,
-                ModifiedDate = DateTime.Now,
-                DeletedDate = DateTime.Now,
-                Instances = Enumerable.Empty<Instance>(),
-            },
-        new Tagg()
-            {
-                Id = Guid.NewGuid(),
-                Key = "TEST TAGG 3",
-                CreatedDate = DateTime.Now,
-                ModifiedDate = DateTime.Now,
-                DeletedDate = DateTime.Now,
-                Instances = Enumerable.Empty<Instance>(),
-            },
-    };
+        var mockRepo = new Mock<ITaggRepository>();
 
-    public static Mock<IBaseRepository<Tagg>> GetBaseRepository()
-    {
-        var mockRepo = new Mock<IBaseRepository<Tagg>>();
-
-        mockRepo.Setup(repo => repo.GetAll()).ReturnsAsync(InitialTaggs);
-        
-        foreach(var tagg in InitialTaggs)
-        {
-            mockRepo.Setup(repo => repo.GetByIdWithNavigationProperties(It.Is<Guid>(id => tagg.Id == id), It.IsAny<Expression<Func<Tagg, object>>>()))
-                    .ReturnsAsync(tagg);
-        }
-
+        mockRepo.Setup(repo => repo.SearchForTagg(It.Is<string>(s => s == "F")))
+                .ReturnsAsync(MockBaseTaggRepository.InitialTaggs.Where(tagg => tagg.Key.Contains("F")));
+    
         return mockRepo;
     }
 
