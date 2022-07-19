39f35166c08cd70e9301673619ecc1e61e434627
Pseudooo <mitchell.wright877@gmail.com>
2022-07-19 20:34:29 +0100

    Testing GetTaggById

---
+++ TaggTimeline.Service.Test/Queries/GetTaggByIdQueryTests.cs
@@ -0,0 +1,49 @@
+
+using Moq;
+using NUnit.Framework;
+using TaggTimeline.Domain.Entities.Taggs;
+using TaggTimeline.Domain.Interface;
+using TaggTimeline.Service.Exceptions;
+using TaggTimeline.Service.Handlers;
+using TaggTimeline.Service.Queries;
+using TaggTimeline.Service.Test.Mocks;
+
+namespace TaggTimeline.Service.Test.Queries;
+
+[TestFixture]
+public class GetTaggByIdQueryTests
+{
+
+    public Mock<IBaseRepository<Tagg>> MockedRepository { get; set; }
+
+    [SetUp]
+    public void SetUp()
+    {   
+        MockedRepository = MockTaggRepository.GetBaseRepository();
+    }
+
+    [Test]
+    public async Task Get_Tagg_By_Id_Should_Return_Tagg()
+    {
+        var id = MockTaggRepository.InitialTaggs[0].Id;
+        var query = new GetTaggByIdQuery() { Id = id };
+        var handler = new GetTaggByIdHandler(MockedRepository.Object);
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOf<Tagg>(result);
+    }
+
+    [Test]
+    public void Get_Tagg_By_Id_Should_Throw_EntityNotFoundException()
+    {
+        var id = Guid.NewGuid();
+        var query = new GetTaggByIdQuery() { Id = id };
+        var handler = new GetTaggByIdHandler(MockedRepository.Object);
+        
+        Assert.ThrowsAsync<EntityNotFoundException>(async () => {
+            var result = await handler.Handle(query, CancellationToken.None);
+        });
+    }
+
+}
