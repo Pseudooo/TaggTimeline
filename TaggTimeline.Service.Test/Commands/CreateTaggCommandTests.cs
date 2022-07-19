bf513145139ad643e5c1a781cdf4e00c4a5134c5
Pseudooo <mitchell.wright877@gmail.com>
2022-07-19 22:03:10 +0100

    Added tests for commands

---
+++ TaggTimeline.Service.Test/Commands/CreateTaggCommandTests.cs
@@ -0,0 +1,36 @@
+
+using Moq;
+using NUnit.Framework;
+using TaggTimeline.Domain.Entities.Taggs;
+using TaggTimeline.Domain.Interface;
+using TaggTimeline.Service.Commands;
+using TaggTimeline.Service.Handlers;
+using TaggTimeline.Service.Test.Mocks;
+
+namespace TaggTimeline.Service.Test.Commands;
+
+[TestFixture]
+public class CreateTaggCommandTests
+{
+
+    public Mock<IBaseRepository<Tagg>> MockedRepository { get; set; }
+
+    [SetUp]
+    public void SetUp()
+    {
+        MockedRepository = MockBaseTaggRepository.GetBaseRepository();
+    }
+
+    [Test]
+    public async Task Create_Tagg_Should_Create_Tagg()
+    {
+        var command = new CreateTaggCommand() { Key = "Tagg" };
+        var handler = new CreateTaggHandler(MockedRepository.Object);
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOf<Tagg>(result);
+        Assert.AreEqual((await MockedRepository.Object.GetAll()).Count(), 4);
+    }
+
+}
