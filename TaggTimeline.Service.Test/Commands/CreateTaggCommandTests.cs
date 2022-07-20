4dfe286e954031a0f5e1dfb330b8fa277a554c25
Pseudooo <mitchell.wright877@gmail.com>
2022-07-20 01:05:19 +0100

    Introduced transactions into get creation handler

--- TaggTimeline.Service.Test/Commands/CreateTaggCommandTests.cs
+++ TaggTimeline.Service.Test/Commands/CreateTaggCommandTests.cs
@@ -1,6 +1,7 @@
 
 using Moq;
 using NUnit.Framework;
+using TaggTimeline.Domain;
 using TaggTimeline.Domain.Entities.Taggs;
 using TaggTimeline.Domain.Interface;
 using TaggTimeline.Service.Commands;
@@ -15,17 +16,20 @@ public class CreateTaggCommandTests
 
     public Mock<IBaseRepository<Tagg>> MockedRepository { get; set; } = null!;
 
+    public Mock<ITransaction> MockedTransaction { get; set; } = null!;
+
     [SetUp]
     public void SetUp()
     {
         MockedRepository = MockBaseTaggRepository.GetBaseRepository();
+        MockedTransaction = MockTransaction.GetTransaction();
     }
 
     [Test]
     public async Task Create_Tagg_Should_Create_Tagg()
     {
         var command = new CreateTaggCommand() { Key = "Tagg" };
-        var handler = new CreateTaggHandler(MockedRepository.Object);
+        var handler = new CreateTaggHandler(MockedRepository.Object, MockedTransaction.Object);
         var result = await handler.Handle(command, CancellationToken.None);
 
         Assert.IsNotNull(result);
--- TaggTimeline.Service/Handlers/CreateTaggHandler.cs
+++ TaggTimeline.Service/Handlers/CreateTaggHandler.cs
@@ -1,5 +1,6 @@
 
 using MediatR;
+using TaggTimeline.Domain;
 using TaggTimeline.Domain.Entities.Taggs;
 using TaggTimeline.Domain.Interface;
 using TaggTimeline.Service.Commands;
@@ -10,19 +11,25 @@ public class CreateTaggHandler : IRequestHandler<CreateTaggCommand, Tagg>
 {
     private readonly IBaseRepository<Tagg> _baseRepository;
 
-    public CreateTaggHandler(IBaseRepository<Tagg> baseRepository)
+    private readonly ITransaction _transaction;
+
+    public CreateTaggHandler(IBaseRepository<Tagg> baseRepository, ITransaction transaction)
     {
         _baseRepository = baseRepository;
+        _transaction = transaction;
     }
 
     public async Task<Tagg> Handle(CreateTaggCommand request, CancellationToken cancellationToken)
     {
+        using var trans = _transaction.InitialiseTransaction();
+        
         var toBeCreated = new Tagg()
         {
             Key = request.Key,
         };
 
         var created = await _baseRepository.AddItem(toBeCreated);
+        
         return created;
     }
 }
