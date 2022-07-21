21489598ca942877dc301be7527d669565e01f2a
Pseudooo <mitchell.wright877@gmail.com>
2022-07-21 11:33:57 +0100

    Started on tests for tagg controller

--- TaggTimeline.WebApi.Test/TaggControllerTests.cs
+++ TaggTimeline.WebApi.Test/TaggControllerTests.cs
@@ -1,9 +1,24 @@
 
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using TaggTimeline.WebApi;
+
 namespace TaggTimeline.WebApi.Test;
 
+[TestFixture]
 public class TaggControllerTests
 {
 
-     
+    [SetUp]
+    public void SetUp()
+    {
+        var webApplicationFactory = new SandboxApplication();
+    }
+
+    [Test]
+    public void Test_Client()
+    {
+        Assert.IsTrue(true);
+    }
 
 }
