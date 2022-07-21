23432912dcd9fee02f3a8dbdab4168e699b0a69e
Pseudooo <mitchell.wright877@gmail.com>
2022-07-21 18:41:31 +0100

    brought tagg into keyed hierarchy

--- TaggTimeline.Domain/Entities/Taggs/Tagg.cs
+++ TaggTimeline.Domain/Entities/Taggs/Tagg.cs
@@ -3,11 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TaggTimeline.Domain.Entities.Taggs;
 
-public class Tagg : MutableDatedEntity
+public class Tagg : KeyedEntity
 {
-    [Required]
-    [MaxLength(255)]
-    public string Key { get; set; } = null!;
     public IEnumerable<Instance> Instances { get; set; } = null!;
     public IEnumerable<Category> Categories { get; set; } = null!;
 }
