a704c33c429517677b53ed97b3e3db32125f77e7
Pseudooo <mitchell.wright877@gmail.com>
2022-07-21 18:47:36 +0100

    Using keyed repository in tagg search handler

--- TaggTimeline.Service/Handlers/SearchForTaggHandler.cs
+++ TaggTimeline.Service/Handlers/SearchForTaggHandler.cs
@@ -9,16 +9,16 @@ namespace TaggTimeline.Service.Handlers;
 
 public class SearchForTaggHandler : IRequestHandler<SearchForTaggQuery, IEnumerable<TaggPreviewModel>>
 {
-    private readonly ITaggRepository _taggRepository;
+    private readonly IKeyedEntityRepository<Tagg> _taggRepository;
 
-    public SearchForTaggHandler(ITaggRepository taggRepository)
+    public SearchForTaggHandler(IKeyedEntityRepository<Tagg> taggRepository)
     {
         _taggRepository = taggRepository;
     }
 
     public async Task<IEnumerable<TaggPreviewModel>> Handle(SearchForTaggQuery request, CancellationToken cancellationToken)
     {
-        var taggs = await _taggRepository.SearchForTagg(request.SearchTerm);
+        var taggs = await _taggRepository.SearchForKey(request.SearchTerm);
 
         var taggPreviews = taggs.Select(tagg => new TaggPreviewModel() { Id = tagg.Id, Key = tagg.Key })
                                 .ToList();
