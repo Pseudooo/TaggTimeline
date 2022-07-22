e418a13967ad1e9875b2621ac567a72d07bf8f35
Pseudooo <mitchell.wright877@gmail.com>
2022-07-22 23:57:53 +0100

    Restructured category mocking stuff

---
+++ TaggTimeline.Service.Test/Mocks/Categories/MockCategoryMapper.cs
@@ -0,0 +1,28 @@
+
+using MapsterMapper;
+using Moq;
+using TaggTimeline.ClientModel.Taggs;
+using TaggTimeline.Domain.Entities.Taggs;
+
+namespace TaggTimeline.Service.Test.Mocks.Categories;
+
+public class MockCategoryMapper : Mock<IMapper>
+{
+    public MockCategoryMapper()
+    {
+        Setup(mapper => mapper.Map<CategoryModel>(It.IsAny<Category>()))
+            .Returns((Category mappedFrom) => {
+                return new CategoryModel()
+                {
+                    Id = mappedFrom.Id,
+                    Key = mappedFrom.Key,
+                    CreatedDate = mappedFrom.CreatedDate,
+                    ModifiedDate = mappedFrom.ModifiedDate,
+                    DeletedDate = mappedFrom.DeletedDate,
+                };
+            });
+
+        Setup(mapper => mapper.Map<IEnumerable<CategoryModel>>(It.IsAny<IEnumerable<Category>>()))
+            .Returns((IEnumerable<Category> mappedFrom) => mappedFrom.Select(category => this.Object.Map<CategoryModel>(category)).ToList());
+    }
+}
