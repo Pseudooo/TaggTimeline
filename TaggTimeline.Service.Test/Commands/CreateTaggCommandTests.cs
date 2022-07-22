7abdbc0f34fd85a55027dbe4aa9a0846ae1cf555
Pseudooo <mitchell.wright877@gmail.com>
2022-07-22 22:27:37 +0100

    Updated tests

--- TaggTimeline.Service.Test/Commands/CreateTaggCommandTests.cs
+++ TaggTimeline.Service.Test/Commands/CreateTaggCommandTests.cs
@@ -1,12 +1,15 @@
 
+using MapsterMapper;
 using Moq;
 using NUnit.Framework;
+using TaggTimeline.ClientModel.Taggs;
 using TaggTimeline.Domain;
 using TaggTimeline.Domain.Entities.Taggs;
 using TaggTimeline.Domain.Interface;
 using TaggTimeline.Service.Commands;
 using TaggTimeline.Service.Handlers;
 using TaggTimeline.Service.Test.Mocks;
+using TaggTimeline.Service.Test.Mocks.Taggs;
 
 namespace TaggTimeline.Service.Test.Commands;
 
@@ -18,22 +21,25 @@ public class CreateTaggCommandTests
 
     public Mock<ITransactionWrapper> MockedTransaction { get; set; } = null!;
 
+    public Mock<IMapper> MockedMapper { get; set; } = null!;
+
     [SetUp]
     public void SetUp()
     {
         MockedRepository = MockKeyedEntityTaggRepository.GetBaseRepository();
         MockedTransaction = MockTransactionWrapper.GetTransaction();
+        MockedMapper = new MockTaggMapper();
     }
 
     [Test]
     public async Task Create_Tagg_Should_Create_Tagg()
     {
         var command = new CreateTaggCommand() { Key = "Tagg" };
-        var handler = new CreateTaggHandler(MockedRepository.Object, MockedTransaction.Object);
+        var handler = new CreateTaggHandler(MockedRepository.Object, MockedTransaction.Object, MockedMapper.Object);
         var result = await handler.Handle(command, CancellationToken.None);
 
         Assert.IsNotNull(result);
-        Assert.IsInstanceOf<Tagg>(result);
+        Assert.IsInstanceOf<TaggModel>(result);
         Assert.AreEqual((await MockedRepository.Object.GetAll()).Count(), 4);
     }
 
