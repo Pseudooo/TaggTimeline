bafdd99756812e2539ccf7eca082352c585b97c4
Pseudooo <mitchell.wright877@gmail.com>
2022-07-22 16:53:08 +0100

    Updated handler to use mapping

--- TaggTimeline.Service.Test/Queries/GetAllTaggsQueryTests.cs
+++ TaggTimeline.Service.Test/Queries/GetAllTaggsQueryTests.cs
@@ -1,4 +1,5 @@
 
+using MapsterMapper;
 using Moq;
 using NUnit.Framework;
 using TaggTimeline.ClientModel.Taggs;
@@ -15,17 +16,19 @@ public class GetAllTaggsQueryTests
 {
 
     public Mock<IKeyedEntityRepository<Tagg>> MockedRepository { get; set; } = null!;
+    public Mock<IMapper> MockedMapper { get; set; } = null!;
     
     [SetUp]
     public void SetUp()
     {
         this.MockedRepository = MockKeyedEntityTaggRepository.GetBaseRepository();
+        this.MockedMapper = MockMapper.GetMapper();
     }
 
     [Test]
     public async Task Get_All_Taggs_Should_Return_Taggs()
     {
-        var handler = new GetAllTaggsHandler(MockedRepository.Object);
+        var handler = new GetAllTaggsHandler(MockedRepository.Object, MockedMapper.Object);
         var result = await handler.Handle(new GetAllTaggsQuery(), CancellationToken.None);
 
         Assert.IsNotNull(result);
