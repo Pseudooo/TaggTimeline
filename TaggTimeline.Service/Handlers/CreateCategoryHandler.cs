16cd696496d63b3e7495b89c6d24fb18e5b7b212
Pseudooo <mitchell.wright877@gmail.com>
2022-07-24 18:58:46 +0100

    Removed transaction from other handlers

--- TaggTimeline.Service/Handlers/CreateCategoryHandler.cs
+++ TaggTimeline.Service/Handlers/CreateCategoryHandler.cs
@@ -11,28 +11,22 @@ namespace TaggTime.Service.Handlers;
 public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, CategoryModel>
 {
     private readonly IBaseRepository<Category> _baseRepository;
-    private readonly ITransactionWrapper _transactionWrapper;
     private readonly IMapper _mapper;
 
-    public CreateCategoryHandler(IBaseRepository<Category> baseRepository, ITransactionWrapper transactionWrapper, IMapper mapper)
+    public CreateCategoryHandler(IBaseRepository<Category> baseRepository, IMapper mapper)
     {
         _baseRepository = baseRepository;
-        _transactionWrapper = transactionWrapper;
         _mapper = mapper;
     }
 
     public async Task<CategoryModel> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
     {
-        await using var t = await _transactionWrapper.Begin();
-
         var toBeCreated = new Category()
         {
             Key = request.Key,
         };
 
         var created = await _baseRepository.AddItem(toBeCreated);
-
-        await t.Commit();
 
         var createdModel = _mapper.Map<CategoryModel>(created);
 
