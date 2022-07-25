d3777d8c6ae5da9aafc54d8ac37b0a636379c92d
Pseudooo <mitchell.wright877@gmail.com>
2022-07-25 14:02:56 +0100

    Set correct password for sandbox database in gh action

--- TaggTimeline.WebApi.Test/SandboxApplication.cs
+++ TaggTimeline.WebApi.Test/SandboxApplication.cs
@@ -36,7 +36,7 @@ public class SandboxApplication : WebApplicationFactory<Program>
             // Add new context with modified configuration
             sc.AddDbContext<DataContext>(opts => 
             {
-                opts.UseNpgsql("User ID=taggserver;Password=Q6%5nWgeN4#9;Host=localhost;Port=5432;Database=taggtimeline;Pooling=true;Connection Lifetime=0;");
+                opts.UseNpgsql("User ID=taggserver;Password=taggtimeline;Host=localhost;Port=5432;Database=taggtimeline;Pooling=true;Connection Lifetime=0;");
             });
 
             using(var scope = sc.BuildServiceProvider().CreateScope())
