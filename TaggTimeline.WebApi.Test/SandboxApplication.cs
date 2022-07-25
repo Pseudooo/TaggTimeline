70f7ff9db3d4bb62b9eca3f1f1ed9c653b59318e
Pseudooo <mitchell.wright877@gmail.com>
2022-07-25 13:16:35 +0100

    Now removing options from DI container

--- TaggTimeline.WebApi.Test/SandboxApplication.cs
+++ TaggTimeline.WebApi.Test/SandboxApplication.cs
@@ -7,15 +7,27 @@ namespace TaggTimeline.WebApi.Test;
 
 public class SandboxApplication : WebApplicationFactory<Program>
 {
-
-    public SandboxApplication() : base()
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        WithWebHostBuilder(builder => {
-            builder.ConfigureServices(sc => {
-                sc.AddDbContext<DataContext>(opts => opts.UseNpgsql("User ID=taggserver;Password=Q6%5nWgeN4#9;Host=localhost;Port=5432;Database=taggtimelinesb;Pooling=true;Connection Lifetime=0;"));
+        builder.ConfigureServices(sc => {
+            var contextDescriptor = sc.Single(desc => desc.ServiceType == typeof(DataContext));
+            sc.Remove(contextDescriptor);
+
+            var contextOptionsDescriptor = sc.Single(desc => desc.ServiceType == typeof(DbContextOptions<DataContext>));
+            sc.Remove(contextOptionsDescriptor);
+
+            sc.AddDbContext<DataContext>(opts => 
+            {
+                opts.UseNpgsql("User ID=taggserver;Password=Q6%5nWgeN4#9;Host=localhost;Port=5432;Database=taggtimelinesandbox;Pooling=true;Connection Lifetime=0;");
             });
-       });
+
+            using(var scope = sc.BuildServiceProvider().CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                var context = provider.GetRequiredService<DataContext>();
+
+                context.Database.EnsureCreated();
+            }
+        });
     }
-
-
 }
