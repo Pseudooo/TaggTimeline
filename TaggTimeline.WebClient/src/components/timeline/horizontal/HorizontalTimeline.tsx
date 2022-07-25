08f7559e191503be144060427bbfc0a7a7a5b2bc
Dan Wells <dantheradarman@gmail.com>
2022-07-25 21:29:07 +0100

    Created a select taggs dropdown

--- TaggTimeline.WebClient/src/components/timeline/horizontal/HorizontalTimeline.tsx
+++ TaggTimeline.WebClient/src/components/timeline/horizontal/HorizontalTimeline.tsx
@@ -1,16 +1,13 @@
 import { Grid, Paper } from "@mui/material";
-import { FunctionComponent, useEffect, useState } from "react";
+import { FunctionComponent, useState } from "react";
 import { TaggPreviewModel } from "../../../api/generated";
 import { useAPI } from "../../../contexts/API";
-import { CheckboxDefinition, CheckboxList } from "../../io/CheckboxList";
+import { SelectTaggsDropdown } from "../../io/custom/SelectTaggsDropdown";
 import { DateRangePicker } from "../../io/DateRangePicker";
 
 export const HorizontalTimeline: FunctionComponent = () => {
   const { taggs } = useAPI();
-  const [selectedTaggs, setSelectedTaggs] = useState<Set<TaggPreviewModel>>(
-    new Set()
-  );
-  const [checkboxes, setCheckboxes] = useState<CheckboxDefinition[]>([]);
+  const [chosenTaggs, setChosenTaggs] = useState<TaggPreviewModel[]>([]);
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [endDate, setEndDate] = useState<Date | null>(new Date());
 
@@ -19,46 +16,15 @@ export const HorizontalTimeline: FunctionComponent = () => {
     setEndDate(newEnd);
   };
 
-  useEffect(() => {
-    // TODO: Make this not loop so many times
-    setCheckboxes((prev) => {
-      return (taggs.value ?? []).map((tagg) => ({
-        label: tagg.key,
-        value: tagg.id,
-        checked: prev.find((i) => i.value === tagg.id)?.checked ?? false,
-      }));
-    });
-  }, [taggs.value]);
-
-  const handleCheckboxChanged = (
-    checkbox: CheckboxDefinition,
-    checked: boolean
-  ) => {
-    const tagg = taggs.value?.find((i) => i.id === checkbox.value);
-    setCheckboxes((prev) => {
-      return [
-        ...prev.map((i) =>
-          i.value === checkbox.value ? { ...i, checked } : i
-        ),
-      ];
-    });
-    if (!tagg) {
-      return;
-    }
-    if (checked) {
-      setSelectedTaggs((prev) => new Set([...prev.values(), tagg]));
-    } else {
-      setSelectedTaggs(
-        (prev) => new Set([...prev].filter((i) => i.id !== checkbox.value))
-      );
-    }
-  };
-
   return (
     <Paper>
       <Grid container>
         <Grid item xs={3} padding={1}>
-          <CheckboxList items={checkboxes} onChange={handleCheckboxChanged} />
+          <SelectTaggsDropdown
+            taggs={taggs.value ?? []}
+            selected={chosenTaggs}
+            onChange={(taggs) => setChosenTaggs(taggs)}
+          />
         </Grid>
         <Grid item xs={9}>
           <Grid
