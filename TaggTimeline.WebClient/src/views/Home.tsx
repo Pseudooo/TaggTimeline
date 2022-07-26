f0b4fb92f1067fd1d7aacbfe633f7e8417d538db
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2022-07-26 01:35:30 +0100

    Add todo for home page regarding route guards

--- TaggTimeline.WebClient/src/views/Home.tsx
+++ TaggTimeline.WebClient/src/views/Home.tsx
@@ -11,6 +11,7 @@ export const Home: FunctionComponent = () => {
   const { token } = useAuth();
   return (
     <Box display="flex" flexDirection="column" gap={1}>
+      {/* @TODO: Should probably investigate into route guards for react */}
       {token ? <TimelineSpeedDial /> : <UserAccountForm formMode="Register" />}
     </Box>
   );
