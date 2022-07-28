7e9e4494f5bbfee166c866394cedfcfb9bd00504
Pseudooo <mitchell.wright877@gmail.com>
2022-07-28 21:22:11 +0100

    Consistency

--- TaggTimeline.Domain/Interface/IUserMappingRepository.cs
+++ TaggTimeline.Domain/Interface/IUserMappingRepository.cs
@@ -3,7 +3,7 @@ using TaggTimeline.Domain.Entities.Users;
 
 namespace TaggTimeline.Domain.Interface;
 
-public interface IUserRepository : IBaseRepository<UserMapping>
+public interface IUserMappingRepository : IBaseRepository<UserMapping>
 {
     Task<UserMapping?> GetByUserId(string userId);
 }
--- TaggTimeline.Domain/Repositories/UserMappingRepository.cs
+++ TaggTimeline.Domain/Repositories/UserMappingRepository.cs
@@ -2,14 +2,15 @@
 using Microsoft.EntityFrameworkCore;
 using TaggTimeline.Domain.Context;
 using TaggTimeline.Domain.Entities.Users;
+using TaggTimeline.Domain.Interface;
 
 namespace TaggTimeline.Domain.Repository;
 
-public class UserMappingRepository : BaseRepository<UserMapping>
+public class UserMappingRepository : BaseRepository<UserMapping>, IUserMappingRepository
 {
     public UserMappingRepository(DataContext context) : base(context)
         { }
-    
+
     public Task<UserMapping?> GetByUserId(string userId)
     {
         return Context.UserMappings.SingleOrDefaultAsync(userMapping => userMapping.UserId == userId);
