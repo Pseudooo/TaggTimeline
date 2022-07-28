d88a012e46c791962a23405214fc51fee82adc5c
Pseudooo <mitchell.wright877@gmail.com>
2022-07-28 21:28:01 +0100

    Added foreign key fields to Tagg entity

--- TaggTimeline.Domain/Entities/Taggs/Tagg.cs
+++ TaggTimeline.Domain/Entities/Taggs/Tagg.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TaggTimeline.Domain.Entities.Users;
+
 namespace TaggTimeline.Domain.Entities.Taggs;
 
 public class Tagg : KeyedEntity
 {
     public IEnumerable<Instance> Instances { get; set; } = null!;
     public IEnumerable<Category> Categories { get; set; } = null!;
+    
+    [ForeignKey("UserMapping")]
+    public Guid UserMappingId { get; set; }
+    [Required]
+    public UserMapping UserMapping { get; set; } = null!;
 }
