44af78d82882831cc2bb38913c46ce549b603aeb
Pseudooo <mitchell.wright877@gmail.com>
2022-07-28 20:34:18 +0100

    A user has their own taggs & categories

--- TaggTimeline.Domain/Entities/Users/UserMapping.cs
+++ TaggTimeline.Domain/Entities/Users/UserMapping.cs
@@ -1,5 +1,6 @@
 
 using System.ComponentModel.DataAnnotations;
+using TaggTimeline.Domain.Entities.Taggs;
 
 namespace TaggTimeline.Domain.Entities.Users;
 
@@ -7,4 +8,7 @@ public class UserMapping : BaseEntity
 {
     [Required]
     public string UserId { get; set; } = null!;
+
+    public IEnumerable<Tagg> Taggs { get; set; } = null!;
+    public IEnumerable<Category> Categories { get; set; } = null!;
 }
