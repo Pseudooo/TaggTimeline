b68cd4fd0fcbffbcb816626751d3b42e023a8303
Pseudooo <mitchell.wright877@gmail.com>
2022-07-30 00:52:15 +0100

    Used json ignore to hide UserId property from swagger

--- TaggTimeline.Service/UserCentricRequest.cs
+++ TaggTimeline.Service/UserCentricRequest.cs
@@ -1,9 +1,11 @@
 
+using System.Text.Json.Serialization;
 using MediatR;
 
 namespace TaggTimeline.Service;
 
 public class UserCentricRequest<TReturn> : IRequest<TReturn>
 {
+    [JsonIgnore]
     public string UserId { get; set; } = null!;
 }
