510a4eeb04a6339b65b6bc47b2d2cf2adbcbf6c6
Pseudooo <mitchell.wright877@gmail.com>
2022-07-29 20:56:22 +0100

    Made Category user owned entity

--- TaggTimeline.Domain/Entities/Taggs/Category.cs
+++ TaggTimeline.Domain/Entities/Taggs/Category.cs
@@ -1,7 +1,7 @@
 
 namespace TaggTimeline.Domain.Entities.Taggs;
 
-public class Category : KeyedEntity
+public class Category : KeyedEntity, IUserOwnedEntity
 {
     public IEnumerable<Tagg> Taggs { get; set; } = null!;
     public string UserId { get; set; } = null!;
