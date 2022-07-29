8a24a6eaec1df8d5942c6deadd78651345047dd4
Pseudooo <mitchell.wright877@gmail.com>
2022-07-29 19:06:30 +0100

    Removed usermapping concept and added direct fk

--- TaggTimeline.MigrationRunner/Scripts/02/00_AddUserMappingIds.sql
+++ TaggTimeline.MigrationRunner/Scripts/02/00_AddUserMappingIds.sql
@@ -1,34 +1,27 @@
 
-CREATE TABLE IF NOT EXISTS "UserMappings" (
-    "Id" uuid NOT NULL,
-    "UserId" text NOT NULL,
-    CONSTRAINT "PK_UserMappings" PRIMARY KEY ("Id"),
-    UNIQUE ("UserId")
-);
+-- Clear tables to add auth stuff without null constraints
+DELETE FROM "Taggs";
+DELETE FROM "Categories";
+DELETE FROM "CategoryTagg";
 
--- Create a dummy user mapping to link existing user specific content too
-INSERT INTO "UserMappings" ("Id", "UserId")
-    VALUES ('00000000-0000-0000-0000-000000000000'::uuid, '0_')
-    ON CONFLICT DO NOTHING;
+
+-- Add user id to Taggs table
+ALTER TABLE "Taggs"
+    ADD COLUMN "UserId" text NOT NULL;
 
 ALTER TABLE "Taggs"
-    ADD COLUMN "UserMappingId" uuid NOT NULL
-    DEFAULT '00000000-0000-0000-0000-000000000000'::uuid;
+    ADD CONSTRAINT "FK_Taggs_Users_UserId" 
+        FOREIGN KEY ("UserId")
+        REFERENCES "AspNetUsers" ("Id")
+        ON DELETE CASCADE;
 
-ALTER TABLE "Taggs" 
-    ALTER COLUMN "UserMappingId"
-    DROP DEFAULT;
 
-ALTER TABLE "Taggs"
-    ADD CONSTRAINT "FK_Taggs_UserMappings_UserMappingId" FOREIGN KEY ("UserMappingId") REFERENCES "UserMappings";
+-- Add user id to categories table
+ALTER TABLE "Categories"
+    ADD COLUMN "UserId" text NOT NULL;
 
 ALTER TABLE "Categories"
-    ADD COLUMN "UserMappingId" uuid NOT NULL
-    DEFAULT '00000000-0000-0000-0000-000000000000'::uuid;
-
-ALTER TABLE "Categories" 
-    ALTER COLUMN "UserMappingId"
-    DROP DEFAULT;
-
-ALTER TABLE "Categories"
-    ADD CONSTRAINT "FK_Categories_UserMappings_UserMappingId" FOREIGN KEY ("UserMappingId") REFERENCES "UserMappings";
+    ADD CONSTRAINT "FK_Categories_Users_UserId" 
+        FOREIGN KEY ("UserId") 
+        REFERENCES "AspNetUsers" ("Id")
+        ON DELETE CASCADE;
