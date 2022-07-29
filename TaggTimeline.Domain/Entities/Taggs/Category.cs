84b17ddef4bbd78e24abdbb869a27a06988fa647
Pseudooo <mitchell.wright877@gmail.com>
2022-07-29 19:33:55 +0100

    Added UserId field to category

--- TaggTimeline.Domain/Entities/Taggs/Category.cs
+++ TaggTimeline.Domain/Entities/Taggs/Category.cs
@@ -4,4 +4,5 @@ namespace TaggTimeline.Domain.Entities.Taggs;
 public class Category : KeyedEntity
 {
     public IEnumerable<Tagg> Taggs { get; set; } = null!;
+    public string UserId { get; set; } = null!;
 }
